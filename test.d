39350e11468e796149d5f4abaf51dc8e1d9d2044
Lance Bachmeier <lance@lancebachmeier.com>
2022-12-02 22:24:39 -0600

    nm

--- test.d
+++ test.d
@@ -10,25 +10,6 @@ mno\tpqr\t789";
   writeln(grep(str, ["--str-eq", "3:789"]));
   writeln("");
   writeln(grep(str, ["--gt", "3:400"]));
-    //~ /* When running in DMD code coverage mode, turn on report merging. */
-    //~ version(D_Coverage) version(DigitalMars)
-    //~ {
-        //~ import core.runtime : dmd_coverSetMerge;
-        //~ dmd_coverSetMerge(true);
-    //~ }
-
-    //~ if (!r[0]) return r[1];
-    //~ version(LDC_Profile)
-    //~ {
-        //~ import ldc.profile : resetAll;
-        //~ resetAll();
-    //~ }
-    //~ try 
-    //~ catch (Exception e)
-    //~ {
-        //~ stderr.writefln("Error [%s]: %s", cmdopt.programName, e.msg);
-        //~ return 1;
-    //~ }
-    return 0;
+  return 0;
 }
   
